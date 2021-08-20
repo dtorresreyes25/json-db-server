21ff074edd96c225fd09e36f506a62df71a5f885
dayron.rafael <dayron.rafael@blueliv.com>
2021-08-19 21:22:19 -0400

    added posts endpoint

--- faker.js
+++ faker.js
@@ -1,8 +1,7 @@
 var faker = require("faker");
 
-function generateCaseStudies() {
-  var cases = [];
-  for (let i = 0; i <= 20; i++) {
+function generateCasesStudies(){
+	 for (let i = 0; i <= 20; i++) {
     var id = faker.datatype.uuid();
     var title = faker.lorem.words();
     var location = faker.address.city();
@@ -25,7 +24,40 @@ function generateCaseStudies() {
       description,
     });
   }
-  return { "case-studies": cases };
 }
 
-module.exports = generateCaseStudies;
+function generatePosts(){
+	var posts = [];
+	 for (let i = 0; i <= 20; i++) {
+    var id = faker.datatype.uuid();
+    var title = faker.lorem.words();
+    var location = faker.address.city();
+    var picture = `${faker.image.city()}?random=${Math.round(
+      Math.random() * 1000
+    )}`;
+    var description = "";
+
+    Array(10)
+      .fill(null)
+      .forEach((i) => {
+        description = description.concat(" ", faker.lorem.paragraphs());
+      });
+
+    posts.push({
+      id,
+      title,
+      location,
+      picture,
+      description,
+    });
+    return posts;
+  }
+}
+
+function generateAPI() {
+  const caseStudies = generateCasesStudies();
+  const posts = generatePosts();
+  return { "case-studies": caseStudies, posts };
+}
+
+module.exports = generateAPI;
