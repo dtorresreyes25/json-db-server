7f7d25ac999aeabd4a1ffaeb3add5de4d693bba6
dayron.rafael <dayron.rafael@blueliv.com>
2021-08-23 19:17:48 -0400

    add podcast track list quite long

--- faker.js
+++ faker.js
@@ -62,6 +62,42 @@ function generatePosts() {
 }
 
 function generatePodcast() {
+  const mockupTracks = [
+    {
+      title: "Brahms: St Anthony Chorale - Theme, Two Pianos Op.56b",
+      picture:
+        "https://is4-ssl.mzstatic.com/image/thumb/Podcasts113/v4/36/8f/43/368f43a6-c97d-f69e-dcae-6c9a3f08b196/mza_3740946593757410045.png/100x100bb.jpg",
+      time: 2200,
+      date: new Date("8/30/2015"),
+      url:
+        "https://www.mfiles.co.uk/mp3-downloads/brahms-st-anthony-chorale-theme-two-pianos.mp3",
+      description:
+        "This week on Game Set Matchup, we had the pleasure of chatting to Super Connector, Nathan Kelleher, Co-founder and Director of leading investment firm True Altitude. Tune in to hear more about how Nathan went from working in logistics at the Olympics to co-founding True Altitude, the advice he would give to his younger self, and what it takes to create successful business partnerships",
+    },
+    {
+      title: "Franz Schubert's Ständchen - Voice (Clarinet) & Piano",
+      picture:
+        "https://is4-ssl.mzstatic.com/image/thumb/Podcasts113/v4/36/8f/43/368f43a6-c97d-f69e-dcae-6c9a3f08b196/mza_3740946593757410045.png/100x100bb.jpg",
+      time: 3000,
+      date: new Date("7/9/2014"),
+      url:
+        "https://www.mfiles.co.uk/mp3-downloads/franz-schubert-standchen-serenade.mp3",
+      description:
+        "This week on Game Set Matchup, we had the pleasure of chatting to Super Connector, Nathan Kelleher, Co-founder and Director of leading investment firm True Altitude. Tune in to hear more about how Nathan went from working in logistics at the Olympics to co-founding True Altitude, the advice he would give to his younger self, and what it takes to create successful business partnerships",
+    },
+  ];
+
+  const tracks = Array(50)
+    .fill(null)
+    .map((_, index) => {
+      let oddOrEven = index % 2 === 0;
+      return {
+        ...mockupTracks[oddOrEven ? 0 : 1],
+        id: index,
+        episode: index,
+      };
+    });
+
   return {
     picture:
       "https://is4-ssl.mzstatic.com/image/thumb/Podcasts113/v4/36/8f/43/368f43a6-c97d-f69e-dcae-6c9a3f08b196/mza_3740946593757410045.png/100x100bb.jpg",
@@ -76,72 +112,7 @@ function generatePodcast() {
       apple: "http://www.apple.com/brahms/",
       google: "http://www.google.com/brahms/",
     },
-    tracks: [
-      {
-        id: 1,
-        episode: 1,
-        title: "Brahms: St Anthony Chorale - Theme, Two Pianos Op.56b",
-        picture:
-          "https://is4-ssl.mzstatic.com/image/thumb/Podcasts113/v4/36/8f/43/368f43a6-c97d-f69e-dcae-6c9a3f08b196/mza_3740946593757410045.png/100x100bb.jpg",
-        time: 2200,
-        date: new Date("8/30/2015"),
-        url:
-          "https://www.mfiles.co.uk/mp3-downloads/brahms-st-anthony-chorale-theme-two-pianos.mp3",
-        description:
-          "This week on Game Set Matchup, we had the pleasure of chatting to Super Connector, Nathan Kelleher, Co-founder and Director of leading investment firm True Altitude. Tune in to hear more about how Nathan went from working in logistics at the Olympics to co-founding True Altitude, the advice he would give to his younger self, and what it takes to create successful business partnerships",
-      },
-      {
-        id: 2,
-        episode: 2,
-        title: "Franz Schubert's Ständchen - Voice (Clarinet) & Piano",
-        picture:
-          "https://is4-ssl.mzstatic.com/image/thumb/Podcasts113/v4/36/8f/43/368f43a6-c97d-f69e-dcae-6c9a3f08b196/mza_3740946593757410045.png/100x100bb.jpg",
-        time: 3000,
-        date: new Date("7/9/2014"),
-        url:
-          "https://www.mfiles.co.uk/mp3-downloads/franz-schubert-standchen-serenade.mp3",
-        description:
-          "This week on Game Set Matchup, we had the pleasure of chatting to Super Connector, Nathan Kelleher, Co-founder and Director of leading investment firm True Altitude. Tune in to hear more about how Nathan went from working in logistics at the Olympics to co-founding True Altitude, the advice he would give to his younger self, and what it takes to create successful business partnerships",
-      },
-      {
-        id: 3,
-        episode: 1,
-        title: "Brahms: St Anthony Chorale - Theme, Two Pianos Op.56b",
-        picture:
-          "https://is4-ssl.mzstatic.com/image/thumb/Podcasts113/v4/36/8f/43/368f43a6-c97d-f69e-dcae-6c9a3f08b196/mza_3740946593757410045.png/100x100bb.jpg",
-        time: 2200,
-        date: new Date("8/30/2015"),
-        url:
-          "https://www.mfiles.co.uk/mp3-downloads/brahms-st-anthony-chorale-theme-two-pianos.mp3",
-        description:
-          "This week on Game Set Matchup, we had the pleasure of chatting to Super Connector, Nathan Kelleher, Co-founder and Director of leading investment firm True Altitude. Tune in to hear more about how Nathan went from working in logistics at the Olympics to co-founding True Altitude, the advice he would give to his younger self, and what it takes to create successful business partnerships",
-      },
-      {
-        id: 4,
-        episode: 2,
-        title: "Franz Schubert's Ständchen - Voice (Clarinet) & Piano",
-        picture: "",
-        time: 3000,
-        date: new Date("7/9/2014"),
-        url:
-          "https://www.mfiles.co.uk/mp3-downloads/franz-schubert-standchen-serenade.mp3",
-        description:
-          "This week on Game Set Matchup, we had the pleasure of chatting to Super Connector, Nathan Kelleher, Co-founder and Director of leading investment firm True Altitude. Tune in to hear more about how Nathan went from working in logistics at the Olympics to co-founding True Altitude, the advice he would give to his younger self, and what it takes to create successful business partnerships",
-      },
-      {
-        id: 5,
-        episode: 1,
-        title: "Brahms: St Anthony Chorale - Theme, Two Pianos Op.56b",
-        picture:
-          "https://is4-ssl.mzstatic.com/image/thumb/Podcasts113/v4/36/8f/43/368f43a6-c97d-f69e-dcae-6c9a3f08b196/mza_3740946593757410045.png/100x100bb.jpg",
-        time: 2200,
-        date: new Date("8/30/2015"),
-        url:
-          "https://www.mfiles.co.uk/mp3-downloads/brahms-st-anthony-chorale-theme-two-pianos.mp3",
-        description:
-          "This week on Game Set Matchup, we had the pleasure of chatting to Super Connector, Nathan Kelleher, Co-founder and Director of leading investment firm True Altitude. Tune in to hear more about how Nathan went from working in logistics at the Olympics to co-founding True Altitude, the advice he would give to his younger self, and what it takes to create successful business partnerships",
-      },
-    ],
+    tracks,
   };
 }
 
